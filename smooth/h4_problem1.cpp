c0bf7032e06f48c34609265fd46486ef7854784e
Wallmountain <Benny5090596@gmail.com>
2021-11-20 21:09:48 +0800

    Finish ok

--- smooth/h4_problem1.cpp
+++ smooth/h4_problem1.cpp
@@ -91,6 +91,7 @@ void* smooth(void *data)
     for(int count = 0; count < NSmooth; count ++)
     {
         for(int i = 0; i<bmpInfo.biHeight; i++)
+        {
             for(int j = *id; j<bmpInfo.biWidth; j+=thread_count)
             {
                 Top = (i+bmpInfo.biHeight-1)%bmpInfo.biHeight;
@@ -102,136 +103,107 @@ void* smooth(void *data)
                 BMPSaveData[i][j].rgbGreen =  (double) (BMPData[i][j].rgbGreen+BMPData[Top][j].rgbGreen+BMPData[Top][Left].rgbGreen+BMPData[Top][Right].rgbGreen+BMPData[Down][j].rgbGreen+BMPData[Down][Left].rgbGreen+BMPData[Down][Right].rgbGreen+BMPData[i][Left].rgbGreen+BMPData[i][Right].rgbGreen)/9+0.5; 
                 BMPSaveData[i][j].rgbRed =  (double) (BMPData[i][j].rgbRed+BMPData[Top][j].rgbRed+BMPData[Top][Left].rgbRed+BMPData[Top][Right].rgbRed+BMPData[Down][j].rgbRed+BMPData[Down][Left].rgbRed+BMPData[Down][Right].rgbRed+BMPData[i][Left].rgbRed+BMPData[i][Right].rgbRed)/9+0.5;  
             }
-
-            pthread_mutex_lock(&mutex);
-            if(counter[count%2] == thread_count - 1)
-            {
-                swap(BMPSaveData,BMPData);
-                counter[(count+1)%2]=0;
-            }
-            counter[count%2]++;
-            pthread_mutex_unlock(&mutex);
-            while(counter[count%2]<thread_count); 
+	}
+        pthread_mutex_lock(&mutex);
+        if(counter[count%2] == thread_count - 1)
+        {
+            swap(BMPSaveData,BMPData);
+            counter[(count+1)%2]=0;
+        }
+        counter[count%2]++;
+        pthread_mutex_unlock(&mutex);
+        while(counter[count%2]<thread_count); 
     }
     return NULL;
 }
 
-/*********************************************************/
-/* 讀取圖檔                                              */
-/*********************************************************/
 int readBMP(char *fileName)
 {
-	//建立輸入檔案物件	
-        ifstream bmpFile( fileName, ios::in | ios::binary );
- 
-        //檔案無法開啟
-        if ( !bmpFile ){
-                cout << "It can't open file!!" << endl;
-                return 0;
-        }
- 
-        //讀取BMP圖檔的標頭資料
-    	bmpFile.read( ( char* ) &bmpHeader, sizeof( BMPHEADER ) );
- 
-        //判決是否為BMP圖檔
-        if( bmpHeader.bfType != 0x4d42 ){
-                cout << "This file is not .BMP!!" << endl ;
-                return 0;
-        }
- 
-        //讀取BMP的資訊
-        bmpFile.read( ( char* ) &bmpInfo, sizeof( BMPINFO ) );
-        
-        //判斷位元深度是否為24 bits
-        if ( bmpInfo.biBitCount != 24 ){
-                cout << "The file is not 24 bits!!" << endl;
-                return 0;
-        }
+    ifstream bmpFile( fileName, ios::in | ios::binary );
 
-        //修正圖片的寬度為4的倍數
-        while( bmpInfo.biWidth % 4 != 0 )
-        	bmpInfo.biWidth++;
+    if ( !bmpFile )
+    {
+        cout << "It can't open file!!" << endl;
+        return 0;
+    }
 
-        //動態分配記憶體
-        BMPSaveData = alloc_memory( bmpInfo.biHeight, bmpInfo.biWidth);
-        
-        //讀取像素資料
-    	//for(int i = 0; i < bmpInfo.biHeight; i++)
-        //	bmpFile.read( (char* )BMPSaveData[i], bmpInfo.biWidth*sizeof(RGBTRIPLE));
-	    bmpFile.read( (char* )BMPSaveData[0], bmpInfo.biWidth*sizeof(RGBTRIPLE)*bmpInfo.biHeight);
-	
-        //關閉檔案
-        bmpFile.close();
- 
-        return 1;
- 
+    bmpFile.read( ( char* ) &bmpHeader, sizeof( BMPHEADER ) );
+
+    if( bmpHeader.bfType != 0x4d42 )
+    {
+        cout << "This file is not .BMP!!" << endl ;
+        return 0;
+    }
+    bmpFile.read( ( char* ) &bmpInfo, sizeof( BMPINFO ) );
+    
+    if ( bmpInfo.biBitCount != 24 )
+    {
+        cout << "The file is not 24 bits!!" << endl;
+        return 0;
+    }
+
+    while( bmpInfo.biWidth % 4 != 0 )
+        bmpInfo.biWidth++;
+
+    BMPSaveData = alloc_memory( bmpInfo.biHeight, bmpInfo.biWidth);
+    //for(int i = 0; i < bmpInfo.biHeight; i++)
+    //bmpFile.read( (char* )BMPSaveData[i], bmpInfo.biWidth*sizeof(RGBTRIPLE));
+    bmpFile.read( (char* )BMPSaveData[0], bmpInfo.biWidth*sizeof(RGBTRIPLE)*bmpInfo.biHeight);
+
+    bmpFile.close(); 
+    return 1;
 }
-/*********************************************************/
-/* 儲存圖檔                                              */
-/*********************************************************/
+
 int saveBMP( char *fileName)
 {
- 	//判決是否為BMP圖檔
-        if( bmpHeader.bfType != 0x4d42 ){
-                cout << "This file is not .BMP!!" << endl ;
-                return 0;
-        }
-        
- 	//建立輸出檔案物件
-        ofstream newFile( fileName,  ios:: out | ios::binary );
- 
-        //檔案無法建立
-        if ( !newFile ){
-                cout << "The File can't create!!" << endl;
-                return 0;
-        }
- 	
-        //寫入BMP圖檔的標頭資料
-        newFile.write( ( char* )&bmpHeader, sizeof( BMPHEADER ) );
+    //判決是否為BMP圖檔
+    if( bmpHeader.bfType != 0x4d42 ){
+        cout << "This file is not .BMP!!" << endl ;
+        return 0;
+    }
 
-	//寫入BMP的資訊
-        newFile.write( ( char* )&bmpInfo, sizeof( BMPINFO ) );
+    //建立輸出檔案物件
+    ofstream newFile( fileName,  ios:: out | ios::binary );
 
-        //寫入像素資料
-        //for( int i = 0; i < bmpInfo.biHeight; i++ )
-        //        newFile.write( ( char* )BMPSaveData[i], bmpInfo.biWidth*sizeof(RGBTRIPLE) );
-        newFile.write( ( char* )BMPSaveData[0], bmpInfo.biWidth*sizeof(RGBTRIPLE)*bmpInfo.biHeight );
+    //檔案無法建立
+    if ( !newFile )
+    {
+        cout << "The File can't create!!" << endl;
+        return 0;
+    }
+	
+    //寫入BMP圖檔的標頭資料
+    newFile.write( ( char* )&bmpHeader, sizeof( BMPHEADER ) );
 
-        //寫入檔案
-        newFile.close();
- 
-        return 1;
- 
+    //寫入BMP的資訊
+    newFile.write( ( char* )&bmpInfo, sizeof( BMPINFO ) );
+    //for( int i = 0; i < bmpInfo.biHeight; i++ )
+    //newFile.write( ( char* )BMPSaveData[i], bmpInfo.biWidth*sizeof(RGBTRIPLE) );
+    newFile.write( ( char* )BMPSaveData[0], bmpInfo.biWidth*sizeof(RGBTRIPLE)*bmpInfo.biHeight );
+
+    newFile.close();
+
+    return 1;
 }
 
-
-/*********************************************************/
-/* 分配記憶體：回傳為Y*X的矩陣                           */
-/*********************************************************/
 RGBTRIPLE **alloc_memory(int Y, int X )
 {        
-	//建立長度為Y的指標陣列
-        RGBTRIPLE **temp = new RGBTRIPLE *[ Y ];
-	    RGBTRIPLE *temp2 = new RGBTRIPLE [ Y * X ];
-        memset( temp, 0, sizeof( RGBTRIPLE ) * Y);
-        memset( temp2, 0, sizeof( RGBTRIPLE ) * Y * X );
+    RGBTRIPLE **temp = new RGBTRIPLE *[ Y ];
+    RGBTRIPLE *temp2 = new RGBTRIPLE [ Y * X ];
+    memset( temp, 0, sizeof( RGBTRIPLE ) * Y);
+    memset( temp2, 0, sizeof( RGBTRIPLE ) * Y * X );
+    for( int i = 0; i < Y; i++){
+        temp[ i ] = &temp2[i*X];
+    }
 
-	//對每個指標陣列裡的指標宣告一個長度為X的陣列 
-        for( int i = 0; i < Y; i++){
-                temp[ i ] = &temp2[i*X];
-        }
- 
-        return temp;
- 
+    return temp; 
 }
-/*********************************************************/
-/* 交換二個指標                                          */
-/*********************************************************/
+
 void swap(RGBTRIPLE *a, RGBTRIPLE *b)
 {
-	RGBTRIPLE *temp;
-	temp = a;
-	a = b;
-	b = temp;
+    RGBTRIPLE *temp;
+    temp = a;
+    a = b;
+    b = temp;
 }
 
